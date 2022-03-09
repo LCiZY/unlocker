86b1a6ca668c7d25f3860d05369b4668f4fd7e49
LCiZY <1443351569@qq.com>
2022-03-09 11:15:28 +0800

    add exe

--- test/unlocker/unlocker.cpp
+++ test/unlocker/unlocker.cpp
@@ -5,15 +5,74 @@
 #include "..\..\unlocker.hpp"
 
 #include <cstdlib>
+#include <wchar.h>
+#include <locale.h>
+
+const wchar_t *GetWC(const char *c)
+{
+	const size_t cSize = strlen(c) + 1;
+	wchar_t* wc = new wchar_t[cSize];
+	mbstowcs(wc, c, cSize);
+
+	return wc;
+}
+
+int version = 2;
 
 // main
 int _tmain(int argc, _TCHAR* argv[])
 {
-	unlocker::File* file = unlocker::Path::Exists(_T("C:\\Users\\zhangwei01\\Desktop\\DllAgent\\x64\\Release\\x64.dll"));
-	if (file) {
-		file->Unlock();
-		delete file;
+	if (version == 1)
+	{
+		tstring path = _T("F:\\projects\\vscodeProjects\\nuxt\\test安安");
+		
+		unlocker::File* file = unlocker::Path::Exists(path);
+		if (file) {
+			file->Unlock();
+			wprintf(L"unlock成功！\n");
+			delete file;
+		}
 	}
-	system("pause");
+	else {
+		setlocale(LC_ALL, "zh-CN");
+
+		const char p[2048] = {};
+		char p1[2048] = {};
+		int j = 0;
+		printf("请输入要解锁的文件(夹)路径：");
+		scanf("%[^\n]", p);
+		for (int i = 0; i < 2048; i++){
+			if (p[i] == '/') {
+				p1[j] = '\\'; 
+			}
+			else 
+				p1[j] = p[i];
+			if (p[i] == '\0')
+				break;
+			j++;
+		}
+
+		const wchar_t* path = GetWC(p1);
+		//tstring path1 = path;
+		////wscanf(L"%s", path1);
+		//tstring path2 = _T("F:\\projects\\vscodeProjects\\nuxt\\test安安");
+		//printf("%d", path1.compare(path2));
+		//wprintf(L"path1: %s---\npath2: %s---\n", path1, path2);
+		//system("pause");
+		//return -2;
+
+		unlocker::File* file = unlocker::Path::Exists(path);
+		if (file) {
+			file->Unlock();
+			wprintf(L"unlock：%s 成功！\n", path);
+			delete file;
+		}
+		else {
+			wprintf(L"文件(夹)：%s 不存在！\n", path);
+			system("pause");
+		}
+		delete path;
+	}
+	
 	return 0;
 }
